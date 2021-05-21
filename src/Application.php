067b3191b8bc1bf4cf0a13a66d7a8b35bff95de5
he ping <847050412@qq.com>
2021-05-21 18:23:35 +0800

    Add Env

--- src/Application.php
+++ src/Application.php
@@ -3,9 +3,11 @@ declare(strict_types = 1);
 
 namespace Serendipity\Job;
 
+use Dotenv\Dotenv;
 use Serendipity\Job\Console\SerendipityJobCommand;
 use Swow\Socket;
-use \Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Application as SymfonyApplication;
+use function Serendipity\Job\Kernel\env;
 
 class  Application extends SymfonyApplication
 {
@@ -13,6 +15,10 @@ class  Application extends SymfonyApplication
      * @var \Swow\Socket
      */
     protected Socket $server;
+    /**
+     * @var \Dotenv\Dotenv
+     */
+    protected Dotenv $dotenv;
 
     public function __construct()
     {
@@ -20,5 +26,13 @@ class  Application extends SymfonyApplication
         $this->addCommands([
             new SerendipityJobCommand()
         ]);
+        $this->initialize();
     }
+
+    public function initialize() : void
+    {
+        $this->dotenv = Dotenv::createUnsafeImmutable(SERENDIPITY_JOB_PATH);
+        $this->dotenv->safeLoad();
+    }
+
 }
--- src/Kernel/functions.php
+++ src/Kernel/functions.php
@@ -3,6 +3,19 @@ declare(strict_types = 1);
 
 namespace Serendipity\Job\Kernel;
 
+if (!function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    function value(mixed $value)
+    {
+        return $value instanceof \Closure ? $value() : $value;
+    }
+}
 if (!function_exists('class_basename')) {
     /**
      * Get the class "basename" of the given object / class.
@@ -41,5 +54,41 @@ if (!function_exists('call')) {
             $result = call_user_func_array($callback, $args);
         }
         return $result;
+    }
+}
+
+if (!function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param string     $key
+     * @param null|mixed $default
+     *
+     * @return array|bool|string|void
+     */
+    function env(string $key, mixed $default = null)
+    {
+        $value = getenv($key);
+        if ($value === false) {
+            return value($default);
+        }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+        if (($valueLength = strlen($value)) > 1 && $value[0] === '"' && $value[$valueLength - 1] === '"') {
+            return substr($value, 1, -1);
+        }
+        return $value;
     }
 }
