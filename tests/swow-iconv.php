6325fb413f8078d2b4501523fae31eb6da5ea498
he ping <847050412@qq.com>
2021-05-25 13:03:38 +0800

    test

---
+++ tests/swow-iconv.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types = 1);
+
+$text = "This is the Euro symbol '€'.";
+
+echo 'Original : ', $text, PHP_EOL;
+echo 'TRANSLIT : ', \iconv('UTF-8', 'ISO-8859-1//TRANSLIT', $text), PHP_EOL;
+exit;
