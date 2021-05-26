28be024b16af2732cd31493d9650fda2779f76f1
he ping <847050412@qq.com>
2021-05-26 13:19:46 +0800

    MySqlPool

--- config/autoload/database.php
+++ config/autoload/database.php
@@ -1,6 +1,28 @@
 <?php
 declare(strict_types = 1);
 
-return [
+use function Serendipity\Job\Kernel\env;
 
+return [
+    'default' => [
+        'host'          => env('DB_HOST', 'localhost'),
+        'port'          => env('DB_PORT', 3306),
+        'database'      => env('DB_DATABASE', 'hyperf'),
+        'username'      => env('DB_USERNAME', 'root'),
+        'password'      => env('DB_PASSWORD', ''),
+        'charset'       => env('DB_CHARSET', 'utf8'),
+        'collation'     => env('DB_COLLATION', 'utf8_unicode_ci'),
+        'prefix'        => env('DB_PREFIX', ''),
+        'maxSpareConns' => 5,
+        'maxConns'      => 10,
+        'options'       => [
+            // 框架默认配置
+            PDO::ATTR_CASE              => PDO::CASE_NATURAL,
+            PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ORACLE_NULLS      => PDO::NULL_NATURAL,
+            PDO::ATTR_STRINGIFY_FETCHES => false,
+            // 如果使用的为非原生 MySQL 或云厂商提供的 DB 如从库/分析型实例等不支持 MySQL prepare 协议的, 将此项设置为 true
+            PDO::ATTR_EMULATE_PREPARES  => false,
+        ],
+    ],
 ];
