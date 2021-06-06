024a3385495f84127263c6f4c11aec5620c3be07
he ping <847050412@qq.com>
2021-06-06 11:05:04 +0800

    tcp

--- tests/socket/Client.php
+++ tests/socket/Client.php
@@ -1,4 +1,52 @@
 <?php
-declare(strict_types = 1);
+class  TcpClient
+{
+    private $server_port;
+    private $server_addr;
+    private $socket_handle;
+
+    public function __construct($port = 8001, $addr = "127.0.0.1")
+    {
+        $this->server_addr = $addr;
+        $this->server_port = $port;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function createSocket()
+    {
+        //创建socket套接字
+        $this->socket_handle = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if (!$this->socket_handle) {
+            //创建失败抛出异常，socket_last_error获取最后一次socket操作错误码，socket_strerror打印出对应错误码所对应的可读性描述
+            throw new Exception(socket_strerror(socket_last_error($this->socket_handle)));
+        } else {
+            echo "create socket successful\n";
+        }
+    }
+
+    public function connectToServer()
+    {
+        $this->createSocket();
+        if (!socket_connect($this->socket_handle, $this->server_addr, $this->server_port)) {
+            echo socket_strerror(socket_last_error($this->socket_handle)) . "\n";
+            exit(1);
+        } else {
+            while (true) {
+                $data = fgets(STDIN);
+                //如果用户输入quit，那么退出程序
+                if (strcmp($data, "quit") == 0) {
+                    break;
+                }
+                socket_write($this->socket_handle, $data);
+            }
+        }
+    }
+}
+
+$client = new TcpClient();
+$client->connectToServer();
+
 
 
--- tests/socket/Server.php
+++ tests/socket/Server.php
@@ -11,7 +11,7 @@ class Server
 
     protected Socket $socket;
 
-    protected function __construct(string $host = '127.0.0.1', int $port = 8001, int $backlog = 128)
+    public function __construct(string $host = '127.0.0.1', int $port = 8001, int $backlog = 128)
     {
         $this->host    = $host;
         $this->port    = $port;
@@ -29,4 +29,63 @@ class Server
             echo "create socket successful\n";
         }
     }
+
+    /**
+     * @throws Exception
+     */
+    private function bindAddr()
+    {
+        if (!socket_bind($this->socket, $this->host, $this->port)) {
+            throw new Exception(socket_strerror(socket_last_error($this->socket_handle)));
+        } else {
+            echo "bind addr successful\n";
+        }
+    }
+
+    private function listen()
+    {
+        if (!socket_listen($this->socket, $this->backlog)) {
+            throw new Exception(socket_strerror(socket_last_error($this->socket_handle)));
+        } else {
+            echo "socket  listen successful\n";
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function accept()
+    {
+        $client_socket_handle = socket_accept($this->socket);
+        if (!$client_socket_handle) {
+            echo "socket_accept call failed\n";
+            exit(1);
+        }
+
+        while (true) {
+            $bytes_num = socket_recv($client_socket_handle, $buffer, 100, 0);
+            if (!$bytes_num) {
+                echo "socket_recv  failed\n";
+                exit(1);
+            }
+
+            echo 'content from client:' . $buffer . "\n";
+        }
+    }
+
+    public function startServer()
+    {
+        try {
+            $this->socketCreate();
+            $this->bindAddr();
+            $this->listen();
+            $this->accept();
+        } catch (Exception $exception) {
+            echo $exception->getMessage() . "\n";
+        }
+    }
 }
+
+$server = new Server();
+$server->startServer();
+
