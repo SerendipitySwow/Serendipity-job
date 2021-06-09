b81a847d19bf98e8b4a25847f2f5996a5a97230e
he ping <847050412@qq.com>
2021-06-09 18:57:25 +0800

    去掉nsq

--- src/Kernel/Nsq/Exception/NsqException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Serendipity\Job\Kernel\Nsq\Exception;
-
-class NsqException extends \RuntimeException
-{
-}
--- src/Kernel/Nsq/Exception/ServerException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Serendipity\Job\Kernel\Nsq\Exception;
-
-final class ServerException extends NsqException
-{
-}
