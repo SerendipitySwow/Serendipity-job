d6f2ed8046cccc1cde516cf15a0c187db7717d12
he ping <847050412@qq.com>
2021-06-17 18:31:21 +0800

    加入任务统计

---
+++ src/Constant/Statistical.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of Serendipity Job
+ * @license  https://github.com/Hyperf-Glory/Serendipity-job/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Serendipity\Job\Constant;
+
+/**
+ * Class Statistical
+ */
+class Statistical
+{
+    public const TASK_SUCCESS = 'task-statistical-success'; //成功
+
+    public const TASK_FAILURE = 'task-statistical-failure'; //失败
+
+    public const TASK_DELAY = 'task-statistical-delay'; //延迟
+
+    public const TASK_RESERVED = 'task-statistical-reserved';
+
+    public const TASK_TERMINATION = 'task-statistical-termination'; //终止
+
+    public const DAG_SUCCESS = 'dag-statistical-success'; //成功
+
+    public const DAG_FAILURE = 'dag-statistical-failure'; //失败
+
+    public const DAG_DELAY = 'dag-statistical-delay'; //延迟
+
+    public const DAG_RESERVED = 'dag-statistical-reserved';
+
+    public const DAG_TERMINATION = 'dag-statistical-termination'; //终止
+}
