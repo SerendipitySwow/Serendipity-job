d4bcbb4823c21812b0309b8b23082e332b892e3d
he ping <847050412@qq.com>
2021-06-22 23:45:57 +0800

    完成任务事件通知

---
+++ src/Constant/Task.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of Serendipity Job
+ * @license  https://github.com/Hyperf-Glory/Serendipity-job/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Serendipity\Job\Constant;
+
+class Task
+{
+    public const TASK_SUCCESS = 2;
+}
