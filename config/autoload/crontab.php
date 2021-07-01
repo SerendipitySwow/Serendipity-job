5d431fa23cf17a3dd646a8209bbb00f7f74d59f8
he ping <847050412@qq.com>
2021-07-01 11:11:35 +0800

    crontab.config

---
+++ config/autoload/crontab.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is part of Serendipity Job
+ * @license  https://github.com/Hyperf-Glory/Serendipity-job/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+return [
+    'enable' => true,
+    'crontab' => [
+    ],
+];
