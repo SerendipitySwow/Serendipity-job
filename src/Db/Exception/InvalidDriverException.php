593684f22a799885e0423dac09ce56011ae53783
he ping <847050412@qq.com>
2021-07-27 14:53:24 +0800

    优化代码

--- src/Db/Exception/InvalidDriverException.php
+++ src/Db/Exception/InvalidDriverException.php
@@ -5,14 +5,6 @@
  */
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @see     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
 
 namespace Serendipity\Job\Db\Exception;
 
