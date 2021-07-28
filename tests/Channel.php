43c245bace48a617f21bb6b9c4559dd839aef5d4
he ping <847050412@qq.com>
2021-07-28 23:51:01 +0800

    test

--- tests/Channel.php
+++ tests/Channel.php
@@ -5,17 +5,29 @@
  */
 
 declare(strict_types=1);
-
+require_once  __DIR__. '/../vendor/autoload.php';
 $channel = new \Swow\Channel();
-\Swow\Coroutine::run(function () use ($channel) {
+$coroutine = \Swow\Coroutine::run(function () use ($channel) {
     try {
-        $clourse = $channel->pop();
+        sleep(1);
+        $clourse = $channel->push(random_int(11111,222222));
         var_dump($clourse);
     } catch (\Throwable $exception) {
         var_dump($exception->getMessage());
     }
 });
-\Swow\Coroutine::run(function () use ($channel) {
-    sleep(3);
-    $channel->push(3);
-});
+$coroutine->kill();
+try {
+    var_dump($channel->pop(2000));
+    sleep(60);
+}catch (Throwable $Throwable){
+
+}
+while (true){
+    echo 'A'.PHP_EOL;
+}
+//\Swow\Coroutine::run(function () use ($channel) {
+//    sleep(3);
+//    $channel->push(3);
+//});
+
