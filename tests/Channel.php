3c73a3e6d0081831e29ad2ec18baae67b4e11a09
he ping <847050412@qq.com>
2021-07-29 18:37:54 +0800

    API.MD

--- tests/Channel.php
+++ tests/Channel.php
@@ -9,22 +9,26 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $channel = new \Swow\Channel();
 $coroutine = \Swow\Coroutine::run(function () use ($channel) {
     try {
+        \Swow\defer(function () {
+            echo 1;
+        });
         sleep(1);
+        file_get_contents('https://www.baidu.com');
         $clourse = $channel->push(random_int(11111, 222222));
         var_dump($clourse);
     } catch (\Throwable $exception) {
         var_dump($exception->getMessage());
     }
 });
+sleep(1);
 $coroutine->kill();
+var_dump($coroutine->isAvailable());
 try {
     var_dump($channel->pop(2000));
     sleep(60);
 } catch (Throwable $Throwable) {
 }
-while (true) {
-    echo 'A' . PHP_EOL;
-}
+
 //\Swow\Coroutine::run(function () use ($channel) {
 //    sleep(3);
 //    $channel->push(3);
