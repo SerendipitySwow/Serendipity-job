6ebcfbf83104331a152cba29c5a41b0a288c95f6
he ping <847050412@qq.com>
2021-07-30 11:34:47 +0800

    format

--- src/Dingtalk/Messages/ActionCard.php
+++ src/Dingtalk/Messages/ActionCard.php
@@ -5,11 +5,6 @@
  */
 
 declare(strict_types=1);
-/**
- * This file is part of Task-Schedule.
- *
- * @license  https://github.com/Hyperf-Glory/Task-Schedule/main/LICENSE
- */
 
 namespace Serendipity\Job\Dingtalk\Messages;
 
