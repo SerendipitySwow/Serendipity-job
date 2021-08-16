a0bf3c3d4af0c8b637362de49175282fabc133f7
he ping <847050412@qq.com>
2021-08-16 15:06:11 +0800

    优化task执行流程

--- tests/str_replace.php
+++ tests/str_replace.php
@@ -1,9 +1,12 @@
 <?php
+/**
+ * This file is part of Serendipity Job
+ * @license  https://github.com/serendipitySwow/Serendipity-job/blob/main/LICENSE
+ */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
-
-$bodytag = str_replace("{date}", date("Y-m-d"), "/10001/{txt}");
+$bodytag = str_replace('{date}', date('Y-m-d'), '/10001/{txt}');
 echo $bodytag;
 
 echo trim("date('y-m-d')");
