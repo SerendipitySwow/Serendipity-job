85c535df84145d15ad47b366f96d360c71bc7baf
he ping <847050412@qq.com>
2021-08-16 22:06:07 +0800

    优化ServerProvider封装Request

--- src/Kernel/Http/Request.php
+++ src/Kernel/Http/Request.php
@@ -8,30 +8,16 @@ declare(strict_types=1);
 
 namespace Serendipity\Job\Kernel\Http;
 
-use Psr\Http\Message\RequestInterface;
 use Swow\Http\Server\Request as SwowRequest;
 
-class Request
+class Request extends SwowRequest
 {
-    /**
-     * @var RequestInterface|SwowRequest
-     */
-    protected RequestInterface|SwowRequest $request;
-
-    public function __construct(RequestInterface $request)
-    {
-        $this->request = $request;
-    }
-
     public function all(): array
     {
         return array_merge(
-            $this->request
-                    ->getQueryParams(),
+            $this->getQueryParams(),
             json_decode(
-                    $this->request
-                        ->getBodyAsString() !== '' ? $this->request
-                        ->getBodyAsString() : '{}',
+                    $this->getBodyAsString() !== '' ? $this->getBodyAsString() : '{}',
                     true,
                     512,
                     JSON_THROW_ON_ERROR
@@ -39,16 +25,23 @@ class Request
         ) ?? [];
     }
 
-    /**
-     * @return mixed
-     */
-    public function __call(string $name, array $arguments)
+    public function post(string $key = null, mixed $default = null): mixed
     {
-        return $this->request->{$name}(...$arguments);
+        $body = json_decode(
+            $this->getBody()
+                ->getContents(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+
+        return $key === null ? $body : $body[$key] ?? $default;
     }
 
-    public function setRequest(SwowRequest $request): void
+    public function get(string $key = null, mixed $default = null): mixed
     {
-        $this->request = $request;
+        $params = $this->getQueryParams();
+
+        return $key === null ? $params : $params[$key] ?? $default;
     }
 }
