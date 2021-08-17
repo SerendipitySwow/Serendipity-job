9c5e3589372b37cac80be248d5696bd790d8e655
he ping <847050412@qq.com>
2021-08-17 14:45:38 +0800

    phpunit

---
+++ tests/HttpTestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of Serendipity Job
+ * @license  https://github.com/serendipitySwow/Serendipity-job/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace SerendipityTest;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class HttpTestCase extends TestCase
+{
+}
