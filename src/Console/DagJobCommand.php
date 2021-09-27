c917f80bbec52168755ca151804b48c568cd1db7
he ping <847050412@qq.com>
2021-09-27 19:38:02 +0800

    v2

---
+++ src/Console/DagJobCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Serendipity\Job\Console;
+
+/**
+ * @todo
+ */
+class DagJobCommand extends Command
+{
+
+    protected function bootStrap() : void
+    {
+        // TODO: Implement bootStrap() method.
+    }
+
+    public function handle() : mixed
+    {
+        // TODO: Implement handle() method.
+    }
+}
