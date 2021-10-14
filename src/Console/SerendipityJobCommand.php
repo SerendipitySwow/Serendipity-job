d6730c5c32fb32c1d0e60405a19dc09dbb1452d5
he ping <847050412@qq.com>
2021-10-14 16:54:59 +0800

    blend

--- src/Console/SerendipityJobCommand.php
+++ src/Console/SerendipityJobCommand.php
@@ -8,8 +8,11 @@ declare(strict_types=1);
 
 namespace Serendipity\Job\Console;
 
+use Hyperf\Utils\ApplicationContext;
+use Serendipity\Job\Contract\StdoutLoggerInterface;
 use Serendipity\Job\Kernel\Provider\KernelProvider;
 use Serendipity\Job\Util\Coroutine as SerendipitySwowCo;
+use Swow\Coroutine;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 /**
@@ -36,6 +39,15 @@ final class SerendipityJobCommand extends Command
         });
 
         return SymfonyCommand::SUCCESS;
+    }
+
+    protected function checkProcess(string $cwd): void
+    {
+        if (file_exists("{$cwd}/.pid.server")) {
+            ApplicationContext::getContainer()->get(StdoutLoggerInterface::class)->error('An already started Job development server has been found.');
+            Coroutine::killAll();
+            exit(255);
+        }
     }
 
     protected function bootStrap(): void
