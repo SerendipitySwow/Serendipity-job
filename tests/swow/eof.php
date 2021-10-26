d720613e76e6f0fed773547ee1c537e19adbb747
he ping <847050412@qq.com>
2021-10-26 18:26:31 +0800

    厕所eof

---
+++ tests/swow/eof.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$socket = new \Swow\Socket(\Swow\Socket::TYPE_TCP);
+$socket->connect('127.0.0.1', 9764);
+for ($i=0;$i<10;$i++) {
+    $socket->sendString($i."\r\n");
+    sleep(5);
+    dump($socket->recvString());
+}
+
