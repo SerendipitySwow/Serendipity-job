91a989bd3766bb0d7e83aaa279bea6b97c9e49d5
he ping <847050412@qq.com>
2021-11-01 16:51:39 +0800

    优化waiiter

--- tests/swow/eof.php
+++ tests/swow/eof.php
@@ -1,14 +1,17 @@
 <?php
+/**
+ * This file is part of Serendipity Job
+ * @license  https://github.com/serendipity-swow/serendipity-job/blob/main/LICENSE
+ */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 require __DIR__ . '/../../vendor/autoload.php';
 
 $socket = new \Swow\Socket(\Swow\Socket::TYPE_TCP);
 $socket->connect('127.0.0.1', 9764);
-for ($i=0;$i<10;$i++) {
-    $socket->sendString($i."\r\n");
+for ($i = 0; $i < 10; $i++) {
+    $socket->sendString($i . "\r\n");
     sleep(5);
     dump($socket->recvString());
 }
-
