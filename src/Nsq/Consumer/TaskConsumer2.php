10b796016f42310220c0293382d849d3b0d56a59
he ping <847050412@qq.com>
2021-12-17 19:39:32 +0800

    .

--- src/Nsq/Consumer/TaskConsumer2.php
+++ src/Nsq/Consumer/TaskConsumer2.php
@@ -4,11 +4,6 @@
  * @license  https://github.com/serendipity-swow/serendipity-job/blob/main/LICENSE
  */
 
-/*
- * This file is part of Serendipity Job
- * @license  https://github.com/serendipity-swow/serendipity-job/blob/main/LICENSE
- */
-
 declare(strict_types=1);
 
 namespace Serendipity\Job\Nsq\Consumer;
