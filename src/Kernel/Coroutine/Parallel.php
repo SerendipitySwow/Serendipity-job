244c5919522dabd80a49e04cf98c4e28f8deecdf
he ping <847050412@qq.com>
2022-01-05 20:19:30 +0800

    fix

--- src/Kernel/Coroutine/Parallel.php
+++ src/Kernel/Coroutine/Parallel.php
@@ -10,6 +10,9 @@ namespace SwowCloud\Job\Kernel\Coroutine;
 
 use Hyperf\Utils\Coroutine as HyperfCo;
 use Swow\Channel;
+use Throwable;
+
+use function sprintf;
 
 class Parallel
 {
@@ -52,7 +55,7 @@ class Parallel
             HyperfCo::create(function () use ($callback, $key, $wg, &$result, &$throwables) {
                 try {
                     $result[$key] = call($callback);
-                } catch (\Throwable $throwable) {
+                } catch (Throwable $throwable) {
                     $throwables[$key] = $throwable;
                 } finally {
                     $this->concurrentChannel && $this->concurrentChannel->pop();
@@ -91,7 +94,7 @@ class Parallel
     {
         $output = '';
         foreach ($throwables as $key => $value) {
-            $output .= \sprintf('(%s) %s: %s' . PHP_EOL . '%s' . PHP_EOL, $key, get_class($value), $value->getMessage(), $value->getTraceAsString());
+            $output .= sprintf('(%s) %s: %s' . PHP_EOL . '%s' . PHP_EOL, $key, get_class($value), $value->getMessage(), $value->getTraceAsString());
         }
 
         return $output;
