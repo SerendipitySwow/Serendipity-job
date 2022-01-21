1c00e9a96f7f888f5e03f39d2fe1b8572d50fb23
he ping <847050412@qq.com>
2022-01-21 15:33:09 +0800

    .

---
+++ src/Kernel/Consul/ConsulAgent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of Swow-Cloud/Job
+ * @license  https://github.com/serendipity-swow/serendipity-job/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace SwowCloud\Job\Kernel\Consul;
+
+use SwowCloud\Consul\AgentInterface;
+
+interface ConsulAgent extends AgentInterface
+{
+}
