1059a81780dc918e8a7505585b8eccbf67819d3f
he ping <847050412@qq.com>
2022-02-08 19:17:20 +0800

    Add Debugger

---
+++ src/Kernel/Swow/Debugger.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of Swow-Cloud/Job
+ * @license  https://github.com/serendipity-swow/serendipity-job/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace SwowCloud\Job\Kernel\Swow;
+
+class Debugger extends \Swow\Debug\Debugger
+{
+    public const SDB = <<<'TEXT'
+ ____                        ____ _                 _           _       _     
+/ ___|_      _______      __/ ___| | ___  _   _  __| |         | | ___ | |__  
+\___ \ \ /\ / / _ \ \ /\ / / |   | |/ _ \| | | |/ _` |_____ _  | |/ _ \| '_ \ 
+ ___) \ V  V / (_) \ V  V /| |___| | (_) | |_| | (_| |_____| |_| | (_) | |_) |
+|____/ \_/\_/ \___/ \_/\_/  \____|_|\___/ \__,_|\__,_|      \___/ \___/|_.__/ 
+TEXT;
+}
