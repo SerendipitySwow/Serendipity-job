b9d1cbfac806d6ba4211c69f6e8ce95cf9b216c1
he ping <847050412@qq.com>
2022-02-08 10:33:10 +0800

    测试协程抛异常

--- tests/swow/coroutine.php
+++ tests/swow/coroutine.php
@@ -6,10 +6,15 @@
 
 declare(strict_types=1);
 
-\Swow\Coroutine::run(function () {
+$coroutine = \Swow\Coroutine::run(function () {
     $coroutine = \Swow\Coroutine::getCurrent();
     \Swow\defer(function () use ($coroutine) {
         var_dump($coroutine->getTrace());
     });
-    var_dump(2);
+//    var_dump(2);
+    // 在协程内部抛出异常defer不会被执行
+    $coroutine->throw(new Exception('11111'));
 });
+// just do not
+$coroutine->throw(new Exception('11111'));
+
