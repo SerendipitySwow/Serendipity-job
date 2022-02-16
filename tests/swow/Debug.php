53e84debd3111744ef8f84f5a5fcc0f7cda4df7e
he ping <847050412@qq.com>
2022-02-16 17:36:15 +0800

    测试watchDog最大执行时间

--- tests/swow/Debug.php
+++ tests/swow/Debug.php
@@ -6,9 +6,20 @@
 
 declare(strict_types=1);
 
-require_once '../vendor/autoload.php';
+use Swow\Coroutine;
+use SwowCloud\RedisLock\RedisLock;
 
-\SwowCloud\Job\Kernel\Swow\Debugger::runOnTTY('debug');
+require_once './bootstrap.php';
+/* 测试redis-lock watchdog最大执行时间 不用给redis 一直续锁*/
+//while (true) {
+//    Coroutine::run(function ()  {
+//        $lock = make(RedisLock::class);
+//        $true = $lock->lock('sdfsdf', 5);
+//    });
+//    sleep(10);
+//}
+
+## \SwowCloud\Job\Kernel\Swow\Debugger::runOnTTY('debug');
 ## vars 可以查看所有变量
 ## p $var 可以随时查看$var的值
 ## p $var=1 可以修改$var的值
@@ -21,7 +32,7 @@ while (true) {
     \Swow\Coroutine::run(function () use ($i) {
 //        echo $i . PHP_EOL;
     });
-    \Swow\Coroutine::run(function () use ($i,&$l) {
+    \Swow\Coroutine::run(function () use ($i, &$l) {
         $l = $i * $i;
 //        echo $l . PHP_EOL;
     });
